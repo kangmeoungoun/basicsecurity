a72d0efeb6b1c200e3fe5752a1c877e9c6e22dc3
kangmeoungoun <kes98202@naver.com>
2021-04-04 15:37:28 +0900

    02) 정적 자원 관리 - WebIgnore 설정

--- src/main/java/io/security/basicsecurity/security/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package io.security.basicsecurity.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -21,6 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Override
